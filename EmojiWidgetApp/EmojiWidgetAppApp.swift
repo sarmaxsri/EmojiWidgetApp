f7885394c18c2daa47ab91a66be06e80e00da0ce
Sarvesh S <sarmaxsri@yahoo.com>
2020-09-30 13:14:12 +0530

    Initial Commit

---
+++ EmojiWidgetApp/EmojiWidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiWidgetAppApp.swift
+//  EmojiWidgetApp
+//
+//  Created by Sarvesh on 30/09/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiWidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
